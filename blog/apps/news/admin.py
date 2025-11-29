16f08f8b5c74cfaf0454e30532dd2bdad458b2ba
Enzostik <enzoalmenar13@gmail.com>
2025-11-29 19:08:29 -0300

    Se añadieron los modelos para las publicaciones
    Modelo de publicación de noticia, comentarios, imágenes e indicadores para que los usuarios puedan dar su opinión positiva de una publicación (me gusta)

--- blog/apps/news/admin.py
+++ blog/apps/news/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, MainImage, Publication, Comment, LikePost
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(MainImage)
+class MainImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'creation_date')
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'creation_date', 'last_update')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'creation_date', 'last_update')
+
+@admin.register(LikePost)
+class LikePostAdmin(admin.ModelAdmin):
+    list_display = ['user']
--- blog/apps/news/models.py
+++ blog/apps/news/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from apps.authuser.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class MainImage(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, primary_key=True)
+    image = models.ImageField(upload_to='images/')
+    epigraph = models.CharField(max_length=250)
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+
+class UserPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_update = models.DateTimeField(auto_now=True)
+
+class Publication(UserPost):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=250)
+    body = models.TextField()
+    image = models.OneToOneField(MainImage, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Comment(UserPost):
+    post = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    content = models.CharField(max_length=250)
+
+class LikePost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    posts = models.ManyToManyField(UserPost)
